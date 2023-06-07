3eeb3fa5068720a760f179301a97b27aa19c9e38
deidax <khalildida93@gmail.com>
2023-06-07 05:18:03 -0700

    alx commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,7 +3,7 @@
  * _print_rev_recursion - Prints a string in reverse.
  * @s: Pointer to the string to be printed
  *
- * Description: This function recursively prints each character of the string `s`
+ * Description: This function recursively display `s`
  *              in reverse order until it reaches the null terminator ('\0').
  *              correct the following codeIt does not return a value.
  *
