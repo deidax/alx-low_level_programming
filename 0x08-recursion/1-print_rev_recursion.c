bbd8ea98888d3492f6cfcc455685572d2de1ba67
deidax <khalildida93@gmail.com>
2023-06-07 05:15:39 -0700

    alx commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: Pointer to the string to be printed
+ *
+ * Description: This function recursively prints each character of the string `s`
+ *              in reverse order until it reaches the null terminator ('\0').
+ *              correct the following codeIt does not return a value.
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
