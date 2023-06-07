38edeb2a669b3e888d25d042c2cc1b413267b8d8
deidax <khalildida93@gmail.com>
2023-06-07 13:42:44 -0700

    alx commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number for which the square root is calculated.
+ *
+ * Description:
+ *	This function recursively calculates and returns the natural square root of
+ *	the number @n. If @n does not have a natural square root, the function
+ *	returns -1 to indicate an error.
+ *
+ * Return: The natural square root of @n, or -1 if an error occurs.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqrt_helper(n, 1));
+}
+
+/**
+ * sqrt_helper - Recursive helper function to find the square root.
+ * @n: The number for which the square root is calculated.
+ * @i: The current value to check for square root.
+ *
+ * Return: The natural square root of @n, or -1 if not found.
+ */
+int sqrt_helper(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (sqrt_helper(n, i + 1));
+}
+
