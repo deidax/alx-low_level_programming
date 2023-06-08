d123ed3bf6ca722e3ad6b2084d0032e0c097606f
deidax <khalildida93@gmail.com>
2023-06-08 01:59:24 -0700

    alx commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,17 @@
 /**
- * sqrt_helper - Recursive helper function to find the square root.
+ * _sqrt_helper - Recursive helper function to find the square root.
  * @n: The number for which the square root is calculated.
  * @i: The current value to check for square root.
  *
  * Return: The natural square root of @n, or -1 if not found.
  */
-int sqrt_helper(int n, int i)
+int _sqrt_helper(int n, int i)
 {
 	if (i * i == n)
 		return (i);
 	if (i * i > n)
 		return (-1);
-	return (sqrt_helper(n, i + 1));
+	return (_sqrt_helper(n, i + 1));
 }
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
@@ -30,6 +30,6 @@ int _sqrt_recursion(int n)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
-	return (sqrt_helper(n, 1));
+	return (_sqrt_helper(n, 1));
 }
 
