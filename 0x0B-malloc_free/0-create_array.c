77365cf59a4983c18233409aa24206757706a73f
deidax <khalildida93@gmail.com>
2023-06-13 07:22:07 -0700

    alx commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+/**
+ * create_array - Creates an array of chars.
+ * @size: The size of the array.
+ * @c: The char to initialize the array with.
+ *
+ * Description:
+ *   This function dynamically allocates an array of chars with the given size.
+ *   Each element of the array is initialized with the specified char.
+ *
+ * Return:
+ *   On success, returns a pointer to the created array.
+ *   On failure or if size is 0, returns NULL.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+	ptr = malloc(sizeof(char) * size);
+	
+	if (ptr == NULL || size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+
+	return (ptr);
+}
