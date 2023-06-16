c99b25c5a30d46206a0f4eeba67cd4a9432377c4
deidax <khalildida93@gmail.com>
2023-06-16 04:39:20 -0700

    alx commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 
 	if (argc <= 1)
 	{
-		printf("%d", 0);
+		printf("%d\n", 0);
 		return (0);
 	}
 	for (i = 1; i < argc; i++)
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 		{
 			if (!isdigit(argv[i][j]))
 			{
-				printf("Error");
+				printf("Error\n");
 				return (1);
 			}
 		}
