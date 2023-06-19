0478b947b09f6db69f01e89cdda43805439bbde9
deidax <khalildida93@gmail.com>
2023-06-19 14:54:34 -0700

    alx commit

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the name of the file it was compiled from.
+ *
+ * Description:
+ *   This program prints the name of the file it was compiled from,
+ *   followed by a new line. It utilizes the __FILE__ predefined macro
+ *   provided by the C standard library to retrieve the file name.
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}
