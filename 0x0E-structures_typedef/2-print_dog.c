84c97b663f6002c7b18086f5abc9053b60270bb5
deidax <khalildida93@gmail.com>
2023-06-20 12:00:41 -0700

    alx commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include "dog.h"
-#ifndef NIL
-#define NIL (a) do { printf(a ": (nil)"); }while(0)
-#endif
 /**
  * print_dog - Prints the content of a struct dog.
  * @d: Pointer to the struct dog to be printed.
@@ -28,6 +25,16 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if(d->name == NULL)
-		NIL("Name");
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age == 0.0)
+		printf("Age: (nil)");
+	else
+		printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
