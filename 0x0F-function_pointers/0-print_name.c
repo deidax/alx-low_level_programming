26c72109ea1266b853f0fd13e1e2a8fbb93b5c06
deidax <khalildida93@gmail.com>
2023-06-21 15:16:58 -0700

    alx commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+/**
+ * print_name - Prints a name using a function pointer.
+ * @name: The name to be printed.
+ * @f: A function pointer to a function that prints the name.
+ *
+ * Description:
+ *      This function takes a name and a function pointer as parameters.
+ *      It calls the function pointed to by @f, passing the name ,
+ *      which prints the name according to a specific format or behavior.
+ *
+ * Return: None.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
