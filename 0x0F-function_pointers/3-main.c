88f4e4edbe6c1a1285cbea7c6affb47149f7399f
deidax <khalildida93@gmail.com>
2023-06-22 13:24:27 -0700

    alx commit

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,12 @@
+#include "3-calc.h"
+/**
+ * main - run the code
+ * @argc: Number of command-line arguments passed
+ * @argv: Array of strings containing the arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+
+}
