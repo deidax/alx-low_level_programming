8d8016c49a7ec67450f0482383cd2e825c44d0ae
deidax <khalildida93@gmail.com>
2023-06-22 13:15:04 -0700

    alx commit

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+/**
+ * get_op_func - Returns a pointer to a function based on the operator.
+ *
+ * @s: The operator string.
+ *
+ * Description:
+ *   The get_op_func function takes an operator string @s as input and
+ *   returns a pointer to a function that corresponds to the provided
+ *   operator. The returned function takes two integer arguments and
+ *   returns an integer.
+ *
+ * Return:
+ *   Pointer to a function that performs the operation corresponding to @s.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	char *opt_char = "+-*/%";
+	void (*opt_cmd[])(int, int) = {op_add, op_sub, op_mul, op_div, op_mod};
+	int c = 0;
+
+	do {
+		if (opt_char == s)
+			return (opt_cmd[c]);
+		c++;
+		opt_char++;
+	}
+	while (opt_char != '\0')
+	return (NULL);
+}
