05cb9a2f49a017a297cfe4544eb1c5708d9b290b
deidax <khalildida93@gmail.com>
2023-06-25 08:05:59 -0700

    alx commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,4 +7,8 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_chat(char c);
+void print_int(int i);
+void print_float(float f);
+void print_string(char *s);
 #endif
