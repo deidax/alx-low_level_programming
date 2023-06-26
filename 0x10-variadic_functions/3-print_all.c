6bc31192587ff36a7f1d3362d3e7ea1ccf837071
deidax <khalildida93@gmail.com>
2023-06-26 05:54:29 -0700

    alx commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 void (*get_format_func(char c))(va_list);
+int get_format(const char * const format);
 /**
  * print_all - Prints anything based on the given format.
  * @format: A list of types of arguments passed to the function.
@@ -29,9 +30,7 @@ void print_all(const char * const format, ...)
 	char f_c;
 	int i = 0;
 
-	if (format == NULL)
-		return;
-	num_args = strlen(format);
+	num_args = get_format(format);
 	va_start(a, format);
 	va_copy(copy, a);
 	while (i < num_args)
@@ -41,10 +40,12 @@ void print_all(const char * const format, ...)
 		{
 			fun_format = get_format_func(f_c);
 			fun_format(copy);
-			printf(", ");
+			if (i < (num_args - 1))
+				printf(", ");
 		}
 		i++;
 	}
 	va_end(a);
-	printf("\n");
+	if (num_args > 0)
+		printf("\n");
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,7 @@
 #endif
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -72,6 +73,23 @@ void print_string(va_list args)
 		printf("%s", v);
 	else
 		printf("(nil)");
+}
+/**
+ * get_format - Get the length of a format string.
+ * @format: The format string.
+ *
+ * Description:
+ *   This function calculates the length of the format string @format.
+ *   If the format string is NULL, the function returns 0.
+ *
+ * Return:
+ *   The length of the format string, or 0 if @format is NULL.
+ */
+int get_format(const char * const format)
+{
+	if (format == NULL)
+		return (0);
+	return (strlen(format));
 }
 /**
  * get_format_func - Returns a pointer to a function based on the format.
