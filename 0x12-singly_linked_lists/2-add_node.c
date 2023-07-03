ae9cd361a23873c4b2c2bcccd40ebf30a2807ce7
deidax <khalildida93@gmail.com>
2023-07-03 03:24:19 -0700

    alx commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,13 +17,17 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *node = malloc(sizeof(list_t));
 
+	if (node == NULL)
+		return (NULL);
 	node->str = strdup(str);
+	if (node->str == NULL)
+	{
+		free(node->str);
+		free(node);
+		return (NULL);
+	}
 	node->len = strlen(str);
 	node->next = *head;
 	*head = node;
-	free(node->str);
-	free(node);
-	if (*head == NULL)
-		return (NULL);
 	return (*head);
 }
