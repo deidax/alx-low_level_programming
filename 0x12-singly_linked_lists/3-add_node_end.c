3cd09f950e511e190e2b765ef7f140798d2f8492
deidax <khalildida93@gmail.com>
2023-07-03 04:40:50 -0700

    alx commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,19 @@
 #include "lists.h"
-
+/**
+ * add_node_end - Adds a new node at the end of a linked list.
+ * @head: Pointer to the head of the linked list.
+ * @str: The string to be stored in the new node.
+ *
+ * Description:
+ *      The add_node_end function creates a new node with the provided string
+ *      and appends it at the end of the linked list pointed to by @head. If
+ *      @head is NULL, the new node becomes the head of the linked list.
+ *      The function returns a pointer to the modified linked list.
+ *
+ * Return:
+ *      - On success, a pointer to the modified linked list.
+ *      - If memory allocation fails or strdup fails, it returns NULL.
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *node = malloc(sizeof(list_t));
