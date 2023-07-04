d5bd1f872db9d7c02e0a51a3a03e023f762452fb
deidax <khalildida93@gmail.com>
2023-07-04 05:30:35 -0700

    alx commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * free_listint2 - Frees the memory occupied by a linked list.
+ * @head: Pointer to the head of the linked list.
+ *
+ * Description:
+ *   The free_listint function traverses the linked list starting from the
+ *   provided head pointer and frees the memory allocated for each node
+ *   and its associated string.
+ *
+ * Return: None.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	if (current == NULL)
+		return;
+	while (current->next != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	free(current);
+	*head = NULL;
+}
