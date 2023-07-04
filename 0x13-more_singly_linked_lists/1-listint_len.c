4db7f49fd373fc2852aba06b53b3ee7e51e568a7
deidax <khalildida93@gmail.com>
2023-07-04 03:51:28 -0700

    alx commit

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_listint - print elements in a linked list.
+ * listint_len - Counts the number of elements in a linked list.
  * @h: Pointer to the head of the list.
  *
  * This function traverses a linked list, counting the number of elements
@@ -8,16 +8,13 @@
  *
  * Return: The number of elements in the list.
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t size = 0;
 
 	if (h == NULL)
 		return (0);
 	for (; h != NULL; size++)
-	{
-		printf("%i\n", h->n);
 		h = h->next;
-	}
 	return (size);
 }
