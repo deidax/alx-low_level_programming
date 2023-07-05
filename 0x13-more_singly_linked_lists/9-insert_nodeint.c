cbce4c572cf6bd19c0b7d134d332487a1a1ce2a9
deidax <khalildida93@gmail.com>
2023-07-05 03:08:39 -0700

    alx commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,14 +17,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	while(current != NULL)
+	while (current != NULL)
 	{
 		if (idx == 0)
 		{
-			right_node = current;
-			current = new_node;
-			current->next = right_node;
-			return (current);
+			new_node->next = *head;
+			*head = new_node;
+			return (*head);
 		}
 		else if (i == idx)
 		{
