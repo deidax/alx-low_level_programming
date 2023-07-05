2c374b674a195b71386460271e8bf845d883a1c5
deidax <khalildida93@gmail.com>
2023-07-05 02:02:34 -0700

    alx commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,8 +1,16 @@
 #include "lists.h"
+/**
+ * get_nodeint_at_index - Returns the node at the specified index
+ * @head: Pointer to of head the the linked list.
+ * @index: Index of the node to retrieve.
+ *
+ * Return: Pointer to the node at the specified index, or if NULL the index is
+ *         out of bounds or the linked list is empty.
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	
+
 	for (; head != NULL; i++)
 	{
 		if (i == index)
