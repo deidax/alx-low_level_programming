c9f274af53380367f1ef9486ae42e2b4a944965b
deidax <khalildida93@gmail.com>
2023-07-06 07:17:55 -0700

    alx commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,18 @@
 #include "main.h"
+/**
+ * binary_to_uint - Converts a binary string to an unsigned integer.
+ * @b: The input binary string.
+ *
+ * Description:
+ *     The binary_to_uint function converts a binary string @b to its
+ *     corresponding unsigned integer value. The binary string must consist
+ *     only of '0' and '1' characters. If the input string @b is NULL or
+ *     contains any characters other than '0' and '1', the function returns 0.
+ *
+ * Return:
+ *     The converted unsigned integer value, or 0 if @b is NULL or contains
+ *     invalid characters.
+ */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int b10 = 0;
@@ -7,7 +21,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	while (*b != '\0')
 	{
-		if (*b != '0' || *b != '1')
+		if ((*b != '0') && (*b != '1'))
 			return (0);
 		b10 = (*b - '0') | (b10 << 1);
 		b++;
