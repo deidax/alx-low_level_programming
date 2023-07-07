7d8985de89aba26e73254cc528ee9206ec573083
deidax <khalildida93@gmail.com>
2023-07-07 01:33:21 -0700

    alx commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,19 @@
 #include "main.h"
+/**
+ * print_binary - Prints the binary representation of an unsigned long integer.
+ * @n: The unsigned long integer to be printed.
+ *
+ * Description:
+ *      The print_binary function takes an unsigned long integer @n and prints
+ *      its binary representation to the standard output using putchar. It
+ *      iterates through each bit of the integer starting from the most
+ *      significant bit and prints '1' or '0' accordingly. If @n is 0 or 1, it
+ *      directly prints the corresponding character. The function also keeps
+ *      track of the rightmost bit found using the RMBF (rightmost bit found)
+ *      flag.
+ *
+ * Return: None.
+ */
 void print_binary(unsigned long int n)
 {
 	unsigned int m = 32768;
@@ -12,7 +27,7 @@ void print_binary(unsigned long int n)
 	{
 		while (m > 0)
 		{
-			if ((m & 1) != 0)
+			if ((m & n) != 0)
 			{
 				_putchar('1');
 				RMBF = 1;
