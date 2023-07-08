e82ce7e37e61115ce409c97553f4c9eaf57fbb4c
deidax <khalildida93@gmail.com>
2023-07-08 16:45:40 -0700

    alx commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void print_times_table(int n)
+{
+	if (n > 15 || n < 0)
+		return;
+	printf("%i,\t", n);
+	print_times_table(n - 1);
+	printf("\n");
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 void print_to_98(int n);
 int _putchar(char c);
+void print_times_table(int n);
