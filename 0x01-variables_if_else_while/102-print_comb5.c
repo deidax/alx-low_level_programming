3733fc3d5f9e3dac3b467dff9c5b4ff56e17e0c0
deidax <khalildida93@gmail.com>
2023-07-08 10:41:04 -0700

    alx commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,20 +2,24 @@
 
 int main(void)
 {
-	int i, j, k;
+	int i, j, k, l;
 
-	for (i = 0; i <=9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = i; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(' ');
-			for (k = j; k <= 9; k++)
+			for (k = 0; k <= 9; k++)
 			{
-				putchar(j + '0');
-				putchar(k + '0');
-				putchar(',');
+				for (l = 0; l <= 9; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
