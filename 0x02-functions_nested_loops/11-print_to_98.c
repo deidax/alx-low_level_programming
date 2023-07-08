3bdf703f4038a357f6b699577f1978f7b67cdafd
deidax <khalildida93@gmail.com>
2023-07-08 12:25:44 -0700

    alx commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_to_98 - Prints all numbers from a given integer to 98, inclusive.
+ * @n: An integer representing the starting point for printing.
+ *
+ * Returns: 0
+ */
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		printf("98\n");
+		return;
+	}
+	printf("%i ,", n);
+	if (n < 98)
+		print_to_98(n + 1);
+	else
+		print_to_98(n - 1);
+}
