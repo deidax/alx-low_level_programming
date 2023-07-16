48608cae477791cb63f84157e7d6d4f350df4303
deidax <khalildida93@gmail.com>
2023-07-16 14:00:29 -0700

    alx commit

--- prep-shell-project/shell.h
+++ prep-shell-project/shell.h
@@ -1,3 +1,8 @@
+typedef struct attribute
+{
+	char *attr;
+	struct attribute *next;
+}
 typedef struct command
 {
         char *cmd;
@@ -6,4 +11,5 @@ typedef struct command
 void shell_prompt(char **cmd);
 void command_av(command **head, char **cmd);
 command *add_command(command **head, const char *cmd);
+attribute *add_attribute(command *cmd, const char *attr);
 size_t print_command(const command *h);
